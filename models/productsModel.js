54510a551cb413c4c65ce88433de9fa554efcafb
Andrew Archer <a.archer@erasolutions.us>
2024-10-12 00:28:01 -0400

    edit product functionality

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,6 +1,6 @@
 const knex = require("../dbConfig");
 
-function createProduct(num_products_per_unit, num_units_available, price_per_unit, name, supplier_id) {
+function createProduct(num_products_per_unit, num_units_available, price_per_product, name, supplier_id) {
     return knex('products')
         .select('*')
         .where({supplier_id: supplier_id, name: name})
@@ -9,7 +9,7 @@ function createProduct(num_products_per_unit, num_units_available, price_per_uni
                 return knex('products').insert({
                     num_products_per_unit: num_products_per_unit,
                     num_units_available: num_units_available,
-                    price_per_unit: price_per_unit,
+                    price_per_product: price_per_product,
                     name: name,
                     supplier_id: supplier_id
                 }).then(function () {
@@ -20,19 +20,71 @@ function createProduct(num_products_per_unit, num_units_available, price_per_uni
 
 }
 
-function postImageLink(key, link, product_id) {
+async function postImageLink(key, link, product_id) {
     return knex('product_image_links')
         .insert({s3_key: key, link: link})
-        .then(function () {
-            return knex('product_image_links').select('id').where({s3_key: key})
+        .then(async function (id) {
+            const image_id = id[0];
+            await knex('product_image_mapping').insert({product_id: product_id, image_id: image_id})
+            return image_id;
         })
         .then(function (id) {
-            const image_id = id[0].id;
-            return knex('product_image_mapping').insert({product_id: product_id, image_id: image_id})
-        })
-        .then(function () {
-            return knex('product_image_links').select('*').where({s3_key: key})
+            return knex('product_image_links').select('*').where({id: id})
         })
 }
 
-module.exports = {createProduct, postImageLink};
+async function getAllProducts(supplier_id) {
+    try {
+        return knex('products')
+            .select('products.id',
+                'products.num_products_per_unit',
+                'products.num_units_available',
+                'products.price_per_product',
+                'products.name',
+                knex.raw('product_image_links.id as image_id'),
+                knex.raw('product_image_links.link as image_link'),
+                knex.raw('product_image_links.s3_key as image_key'))
+            .where({supplier_id: supplier_id})
+            .leftJoin('product_image_mapping', 'products.id', 'product_image_mapping.product_id')
+            .leftJoin('product_image_links', 'product_image_mapping.image_id', 'product_image_links.id')
+            .orderBy('products.id', "desc")
+
+    } catch (error) {
+        return error.message;
+    }
+}
+
+async function updateProduct(product_id, num_products_per_unit, price_per_product, name, old_image_key, new_image_key, new_image_link) {
+    return knex('products')
+        .where({id: product_id})
+        .update({
+            num_products_per_unit: num_products_per_unit,
+            price_per_product: price_per_product,
+            name: name
+        })
+        .then(function () {
+            return knex('product_image_links').where({s3_key: old_image_key})
+                .update({
+                    s3_key: new_image_key,
+                    link: new_image_link
+                })
+                .then(function () {
+                    return knex.select('products.id',
+                        'products.num_products_per_unit',
+                        'products.num_units_available',
+                        'products.price_per_product',
+                        'products.name',
+                        knex.raw('product_image_links.id as image_id'),
+                        knex.raw('product_image_links.link as image_link'),
+                        knex.raw('product_image_links.s3_key as image_key'))
+                        .from('products')
+                        .where('products.id', product_id)
+                        .leftJoin('product_image_mapping', 'products.id', 'product_image_mapping.product_id')
+                        .leftJoin('product_image_links', 'product_image_mapping.image_id', 'product_image_links.id')
+                })
+        })
+
+}
+
+
+module.exports = {createProduct, postImageLink, getAllProducts, updateProduct};
--- routes/products.js
+++ routes/products.js
@@ -11,30 +11,81 @@ router.post("/createProduct", upload.single("file"), async (req, res) => {
         const filePath = req.file.path;
         const fileBody = fs.createReadStream(filePath);
         const fileName = req.file.originalname;
-        const key = ftpModel.createKey('documents', fileName);
+        const key = ftpModel.createKey('product_images', fileName);
         const url = ftpModel.createURL(key);
-        const {num_products_per_unit, num_units_available, price_per_unit, name, supplier_id} = req.body;
+        const {num_products_per_unit, num_units_available, price_per_product, name} = req.body;
+        const supplier_id = req.user.id;
 
         await ftpModel.putObjectInBucket(key, fileBody);
 
-        const product = await productsModel.createProduct(num_products_per_unit, num_units_available, price_per_unit, name, supplier_id);
-        if (!product) res.status(400).json({message: "Cannot have two products with the same name"})
-        let image;
-        if (req.file) image = await productsModel.postImageLink(key, url, product[0].id);
+        const product = await productsModel.createProduct(num_products_per_unit, num_units_available, price_per_product, name, supplier_id);
+        if (!product) {
+            res.status(400).json({message: "Cannot have two products with the same name"})
+            return
+        }
+        let image = await productsModel.postImageLink(key, url, product[0].id);
 
-        res.status(201).json({
-            product,
-            image
-        })
 
         fs.unlink(filePath, function (err) {
             if (err) return console.log(err);
             console.log('File deleted from server storage successfully');
         });
 
+        res.status(201).json({
+            product: {
+                id: product[0].id,
+                name: product[0].name,
+                num_products_per_unit: product[0].num_products_per_unit,
+                num_units_available: product[0].num_units_available,
+                price_per_product: product[0].price_per_product,
+                image_id: image[0].id,
+                image_key: image[0].s3_key,
+                image_link: image[0].link
+            }
+        })
     } catch (error) {
         res.status(500).json(error.message);
     }
 });
 
+router.get('/getProducts', async (req, res) => {
+    try {
+        let products = await productsModel.getAllProducts(req.user.id);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+})
+
+router.post('/updateProduct', upload.single("file"), async (req, res) => {
+    let filePath, url, key, fileName, fileBody;
+    const {old_image_key, name, price_per_product, num_products_per_unit, product_id, old_image_link} = req.body;
+    let updatedProduct;
+
+    try {
+        if (req.file) {
+            filePath = req.file.path;
+            fileBody = fs.createReadStream(filePath);
+            fileName = req.file.originalname;
+            key = ftpModel.createKey('product_images', fileName);
+            url = ftpModel.createURL(key);
+            await ftpModel.putObjectInBucket(key, fileBody).then(function () {
+                return ftpModel.deleteObjectFromBucket(old_image_key);
+            });
+
+            fs.unlink(filePath, function (err) {
+                if (err) return console.log(err);
+                console.log('File deleted from server storage successfully');
+            });
+            updatedProduct = await productsModel.updateProduct(product_id, num_products_per_unit, price_per_product, name, old_image_key, key, url);
+        } else {
+            updatedProduct = await productsModel.updateProduct(product_id, num_products_per_unit, price_per_product, name, old_image_key, old_image_key, old_image_link);
+        }
+
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json(error.message);
+    }
+})
 module.exports = router;
